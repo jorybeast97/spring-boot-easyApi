8b64eaff294906628f4b57dff21d531c39fd0a05
JoryB <489282384@qq.com>
2019-12-30 21:49:27 +0800

    2019.12.30

--- src/main/java/com/magnolia/springbooteasyapi/core/Service.java
+++ src/main/java/com/magnolia/springbooteasyapi/core/Service.java
@@ -1,10 +1,13 @@
 package com.magnolia.springbooteasyapi.core;
 
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * @author 范昊
  * service层抽象方法
  */
 @org.springframework.stereotype.Service
+@Transactional(rollbackFor = Exception.class)
 public interface Service {
 
     
